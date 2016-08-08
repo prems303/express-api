733b90077ce642abb03ce33bb094e94d1abe0594
Jana Winter <janawhitta@gmail.com>
2016-08-08 15:03:47 +1200

    api

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var path = require('path')
 
 var index = require('./routes/index')
 
+var routes = require('./routes')
+// app.get('/widgets/:id', routes.getWidget)
+
 var PORT = 3000
 
 var app = express()
@@ -13,7 +16,23 @@ app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(bodyParser.json())
+
 app.get('/', index.get)
+app.get('/users', routes.getUsers)
+app.get('/users/:id', routes.getUser)
+app.delete('/users/:id', routes.delUser)
+// app.get('/widget', routes.getWidget)
+
+// app.get('/widgets/:id', function (req, res){
+//   var id = req.params.id
+//   res.json({id: id, name: 'Good widget'})
+// })
+
+// app.get('/users/:id', function (req, res){
+//   var id = req.params.id
+//   res.json({id: id, name: 'Good users'})
+// })
 
 app.listen(PORT, function () {
   console.log('Listening on port', PORT)
--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,81 @@ var development = require('../knexfile').development
 var knex = require('knex')(development)
 
 module.exports = {
-  get: get
+  get: get,
+  getUsers: getUsers,
+  getUser: getUser,
+  delUser: delUser
+  // getWidget: getWidget
 }
 
 function get (req, res) {
   knex('users')
     .select()
     .then(function (users) {
-      res.render('index', { users: users })
+      res.json({id: id, name: 'Better Users'})
     })
     .catch(function (err) {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
+      res.status(200).send('DATABASE ERROR: ' + err.message)
     })
 }
+//
+// function getWidget (req, res) {
+//  var id = req.params.id
+//  res.json({id: id, name: 'Better widget'})
+// }
+//
+function getUsers (req, res) {
+  knex('users')
+  .select()
+  .then(function (users){
+     res.json(users)
+  })
+ .catch(function (err) {
+   res.status(200).send('database something: ' + err.message)
+ })
+}
+
+function getUser (req, res) {
+  var id = req.params.id
+  knex('users')
+  .select()
+  .where({
+    id:id
+  })
+  .then(function (user){
+    res.json(user)
+  })
+  .catch(function (err) {
+    res.status(500).send('database something: ' + err.message)
+  })
+}
+
+function delUser (req, res) {
+  var id = req.params.id
+  knex('users')
+  .del()
+  .where({
+    id: id
+  })
+  .then (function (){
+    res.status(200).send('record delted')
+  })
+  .catch(function (err) {
+    res.status(200).send('database something: ' + err.message)
+  })
+}
+//
+// function getUsers (req, res) {
+//   $.ajax('/users', {
+//     success: function (users) {
+//       // users is the response data
+//       users.forEach(function (users) {
+//         $('#users').append('<p>' + users.name + '</p>')
+//       })
+//     },
+//     error: function (jqxhr) {
+//       // jqxhr is the XMLHTTPResponse object
+//       $(200).text('Error: ' + jqxhr.responseText)
+//     }
+//   })
+// }
